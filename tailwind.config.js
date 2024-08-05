96f1b6af1edef4c33dfcbd7dddfcce57fa458d5d
jakeunsted <jake.unsted@gmail.com>
2024-08-05 20:30:05 +0100

    fix messed up colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  important: true,
   content: [
     "./components/**/*.{js,vue,ts}",
     "./layouts/**/*.vue",
@@ -9,12 +10,13 @@ export default {
     "./error.vue",
   ],
   theme: {
-    extend: {},
-    colors: {
-      'primary': '#3FC0F3',
-      'secondary': '#001011',
-      'accent': '#0D9BD3'
-    }
+    extend: {
+      colors: {
+        'primary': '#3FC0F3',
+        'secondary': '#001011',
+        'accent': '#0D9BD3'
+      }
+    },
   },
   plugins: [],
 }
