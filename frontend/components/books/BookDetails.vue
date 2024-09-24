b802d807cb30770128dfe8f6ff659f2eb8b7002a
jakeunsted <jake.unsted@gmail.com>
2024-09-24 13:52:57 +0100

    update and use BookDetails component

--- frontend/components/books/BookDetails.vue
+++ frontend/components/books/BookDetails.vue
@@ -1,94 +1,143 @@
 <template>
-  <div class="w-full max-w-md text-center">
-    <div class="bg-zinc-900 shadow-lg rounded-lg overflow-hidden">
-      <img :src="bookImage" alt="Book Cover" class="h-64 object-cover inline" />
-      <div class="p-4">
-        <h2 class="text-2xl font-bold text-white">{{ book.title }}</h2>
-        <h3 
-          v-if="book.authors && book.authors.length" 
-          class="text-lg text-gray-300"
-        >
-          {{ book.authors.join(', ') }}
-        </h3>
-        <p class="mt-2">
-          {{ truncatedDescription }}
-          <span 
-            v-if="!showFullDescription"
-            class="text-blue-500 cursor-pointer underline decoration-wavy"
-            @click="toggleDescription"
-          >
-            Show More
-          </span>
-          <span 
-            v-if="showFullDescription"
-            class="text-blue-500 cursor-pointer underline decoration-wavy"
-            @click="toggleDescription"
-          >
-            Show Less
-          </span>
-        </p>
-        <p class="mt-2 text-white">
-          <strong>Page Count:</strong> 
-          {{ book.pageCount }}
-        </p>
-        <p class="mt-2 text-white">
-          <strong>Categories:</strong> 
-          {{ book.categories ? book.categories.join(', ') : 'N/A' }}
-        </p>
-        <div class="flex justify-center grid">
-          <v-btn
-            v-if="!aiSearch"
-            @click="findRecommendations"
-            class="bg-primary my-4"
-          >
-            Find Similar
-          </v-btn>
-          <v-btn
-            @click="clearSelection" 
-            class="bg-primary"
-          >
-            Back to Search
-          </v-btn>
-        </div>
+  <div class="w-full text-center p-4" v-if="bookDetails && !loading">
+    <!-- Image section with title, author, and page count -->
+    <div class="flex mb-5">
+      <v-card class="max-w-36" rounded="xl" elevation="10" border="xl">
+        <img :src="bookImage" alt="Book Cover" class="object-cover inline" />
+      </v-card>
+      <div>
+        <v-card :flat="true" color="transparent" class="text-left">
+          <v-card-item>
+            <v-card-title class="text-wrap pb-2">
+              {{ bookDetails.volumeInfo.title || 'No Title Available' }}
+            </v-card-title>
+            <v-card-text class="p-0">
+              {{ primaryAuthor }}
+            </v-card-text>
+            <v-card-text class="p-0">
+              {{ pageCount }}
+            </v-card-text>
+          </v-card-item>
+        </v-card>
       </div>
     </div>
+
+    <!-- Categories -->
+    <h1 class="text-left text-xl">Categories</h1>
+    <div class="flex flex-wrap">
+      <v-chip
+        v-for="category in categories"
+        :key="category"
+        class="m-1"
+        color="primary"
+        rounded="xl"
+        label
+      >
+        {{ category }}
+      </v-chip>
+    </div>
+
+    <!-- Description -->
+    <h1 class="text-left text-xl">Description</h1>
+    <v-card :flat="true" color="transparent" class="text-left">
+      <v-card-text>
+        <p v-if="truncatedDescription">
+          {{ truncatedDescription }}
+          <span
+            v-if="hasLongDescription"
+            @click="toggleDescription"
+            class="text-blue-500 cursor-pointer"
+          >
+            {{ showFullDescription ? 'show less' : 'show more' }}
+          </span>
+        </p>
+        <p v-else>No description available</p>
+      </v-card-text>
+    </v-card>
+
+    <slot name="content"></slot>
   </div>
 </template>
 
 <script setup>
-import { computed, defineProps, defineEmits } from 'vue';
+import { htmlToText } from 'html-to-text';
 
+/**
+ * @prop {Object} bookDetails - The book details object from google api
+ * @prop {String} defaultImage
+ * @prop {Boolean} loading
+ */
 const props = defineProps({
-  book: Object,
-  showFullDescription: Boolean,
-  aiSearch: Boolean,
+  bookDetails: {
+    type: Object,
+    required: true,
+  },
+  defaultImage: {
+    type: String,
+    default: '/assets/default_book.jpg',
+  },
+  loading: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-const emit = defineEmits(['clear']);
+const { bookDetails, defaultImage, loading } = toRefs(props);
 
-const defaultImage = '../../assets/default_book.jpg';
-const bookImage = computed(() => {
-  return props.book.image ? props.book.image : defaultImage;
+const showFullDescription = ref(false);
+const bookImage = ref('');
+const categories = ref([]);
+const truncatedDescription = ref('');
+const plainTextDescription = ref('');
+const hasLongDescription = ref(false);
+
+const primaryAuthor = computed(() => {
+  const authors = bookDetails.value.volumeInfo.authors;
+  return authors && authors.length > 0 ? authors[0] : 'Unknown Author';
 });
 
-const truncatedDescription = computed(() => {
-  if (
-    props.showFullDescription || !props.book
-  ) return props.book.description || '';
-  const description = props.book.description || '';
-  return description.length > 200 ?
-    `${description.slice(0, 200)}...` : description;
+const pageCount = computed(() => {
+  const count = bookDetails.value.volumeInfo.pageCount;
+  return count ? `${count} Pages` : 'Page Count Not Available';
 });
 
 const toggleDescription = () => {
-  emit('toggle-description');
+  showFullDescription.value = !showFullDescription.value;
+  updateTruncatedDescription();
 };
 
-const clearSelection = () => {
-  emit('clear');
+const updateTruncatedDescription = () => {
+  const description = bookDetails.value.volumeInfo?.description || '';
+
+  plainTextDescription.value = htmlToText(description, {
+    wordwrap: false,
+    preserveNewlines: false,
+  });
+
+  hasLongDescription.value = plainTextDescription.value.length > 200;
+
+  truncatedDescription.value = showFullDescription.value
+    ? plainTextDescription.value
+    : hasLongDescription.value
+      ? `${plainTextDescription.value.slice(0, 200)}...`
+      : plainTextDescription.value;
 };
 
-const findRecommendations = () => {
-  emit('findSimilar', props.book);
+const generateCategories = () => {
+  const uniqueCategories = new Set();
+
+  (bookDetails.value.volumeInfo.categories || []).forEach((categoryString) => {
+    const splitCategories = categoryString.split('/').map((cat) => cat.trim());
+    splitCategories.forEach((cat) => uniqueCategories.add(cat));
+  });
+
+  categories.value = Array.from(uniqueCategories).slice(0, 5);
 };
+
+onMounted(() => {
+  bookImage.value =
+    bookDetails.value.volumeInfo?.imageLinks?.thumbnail || defaultImage;
+  generateCategories();
+  updateTruncatedDescription();
+});
 </script>
--- frontend/pages/books/[userid]/[id].vue
+++ frontend/pages/books/[userid]/[id].vue
@@ -1,65 +1,10 @@
 <template>
-  <div class="w-full text-center p-4" v-if="book && !loading">
-    <!-- Image section with title, author and page count -->
-    <div class="flex mb-5">
-      <v-card
-        class="max-w-36"
-        rounded="xl"
-        elevation="10"
-        border="xl"
-      >
-        <img :src="bookImage" alt="Book Cover" class="object-cover inline" />
-      </v-card>
-      <div>
-        <v-card flat=true color="transparent" class="text-left">
-          <v-card-item>
-            <v-card-title class="text-wrap pb-2">
-              {{ bookDetails.volumeInfo.title }}
-            </v-card-title>
-            <v-card-text class="p-0">
-              {{ bookDetails.volumeInfo.authors[0] }}
-            </v-card-text>
-            <v-card-text class="p-0">
-              {{ bookDetails.volumeInfo.pageCount }} Pages
-            </v-card-text>
-          </v-card-item>
-        </v-card>
-      </div>
-    </div>
-
-    <!-- Categories -->
-    <h1 class="text-left text-xl">Categories</h1>
-    <div 
-      class="flex flex-wrap"
-    >
-      <v-chip
-        v-for="category in categories"
-        :key="category"
-        class="m-1"
-        color="primary"
-        rounded="xl"
-        label
-      >
-        {{ category }}
-      </v-chip>
-    </div>
-
-    <!-- Description -->
-    <h1 class="text-left text-xl">Description</h1>
-    <v-card flat="true" color="transparent" class="text-left">
-      <v-card-text>
-        <p v-if="truncatedDescription">
-          {{ truncatedDescription }}
-          <span
-            v-if="truncatedDescription.length > 200"
-            @click="toggleDescription" class="text-blue-500 cursor-pointer"
-          >
-            {{ showFullDescription ? 'show less' : 'show more' }}
-          </span>
-        </p>
-        <p v-else>No description available</p>
-      </v-card-text>
-    </v-card>
+  <div v-if="!loading">
+    <BookDetails
+      :bookDetails="bookDetails"
+      :defaultImage="defaultImage"
+      :loading="loading"
+    />
   </div>
 </template>
 
@@ -69,37 +14,22 @@ definePageMeta({
   layout: 'book',
 });
 
-import { htmlToText } from 'html-to-text';
 import { useBookStore } from '@/stores/useBookStore';
+import BookDetails from '@/components/books/BookDetails.vue';
 
 const route = useRoute();
 const bookStore = useBookStore();
 
 const book = ref(null);
-const bookDetails = ref(null);
-const categories = ref([]);
-const showFullDescription = ref(false);
-// const aiSearch = ref(false);
+const bookDetails = ref({});
 const loading = ref(true);
 
-const defaultImage = '../../assets/default_book.jpg';
-const bookImage = ref(defaultImage);
-const truncatedDescription = ref('');
-
-const toggleDescription = () => {
-  showFullDescription.value = !showFullDescription.value;
-  updateTruncatedDescription();
-};
-
-// const findRecommendations = () => {
-//   console.log('not done yet');
-// };
+const defaultImage = '/assets/default_book.jpg';
 
 const fetchBook = async (userId, bookId) => {
   const bookFromStore = bookStore.getBookById(bookId);
   if (bookFromStore) {
     book.value = bookFromStore;
-    return;
   } else {
     try {
       const response = await useMyFetch(`/users-books/${userId}/${bookId}`);
@@ -110,53 +40,14 @@ const fetchBook = async (userId, bookId) => {
   }
 };
 
-const updateTruncatedDescription = () => {
-  const description = bookDetails.value?.volumeInfo?.description || '';
-
-  const plainTextDescription = htmlToText(description, {
-    wordwrap: false,
-    preserveNewlines: false,
-  });
-
-  // If showing the full description, use the plain text directly
-  if (showFullDescription.value) {
-    truncatedDescription.value = plainTextDescription;
-  } else {
-    // Truncate if the plain text description exceeds 200 characters
-    truncatedDescription.value = plainTextDescription.length > 200 
-      ? `${plainTextDescription.slice(0, 200)}...` 
-      : plainTextDescription;
-  }
-};
-
-const generateCategories = () => {
-  const uniqueCategories = new Set();
-
-  bookDetails.value.volumeInfo.categories.forEach(categoryString => {
-    const splitCategories = categoryString.split('/').map(cat => cat.trim());
-    splitCategories.forEach(cat => uniqueCategories.add(cat));
-  });
-
-  categories.value = Array.from(uniqueCategories).slice(0, 5);
-  console.log('categories:', categories.value);
-};
-
-
-watch(book, (newBook) => {
-  if (newBook) {
-    bookImage.value = 
-      newBook.book?.bookDetails?.volumeInfo?.imageLinks?.thumbnail 
-      || defaultImage;
-    bookDetails.value = newBook.book.bookDetails;
-    updateTruncatedDescription();
-    generateCategories();
-  }
-});
-
 onMounted(async () => {
   const bookId = route.params.id;
   const userId = route.params.userid;
   await fetchBook(userId, bookId);
+  if (book.value) {
+    bookDetails.value = book.value?.book?.bookDetails;
+    console.log('Book Details:', bookDetails.value);
+  }
   loading.value = false;
 });
 </script>
