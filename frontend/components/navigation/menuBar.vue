611240c85da9cbe4b9d8cedbd7e299bc9479f881
jakeunsted <jake.unsted@gmail.com>
2024-09-24 14:56:27 +0100

    customisable menubar component

--- frontend/components/navigation/menuBar.vue
+++ frontend/components/navigation/menuBar.vue
@@ -1,26 +1,26 @@
 <template>
   <div>
-    <v-navigation-drawer 
+    <!-- Navigation Drawer -->
+    <v-navigation-drawer
       v-model="showDrawer"
       location="bottom"
       :class="[
         'drawer w-5/6 left-1/2 transform -translate-x-1/2 bg-background',
-        showDrawer ? 'z-40 bottom-0' : '-bottom-96'
+        showDrawer ? 'z-40 bottom-0' : '-bottom-96',
       ]"
     >
       <v-container class="flex justify-center items-center">
-        <v-sheet 
+        <v-sheet
           v-touch:swipe.down="onExitClick"
           :height="7"
           :width="100"
-          color=""
           class="bg-grey z-50 rounded-full"
         ></v-sheet>
       </v-container>
       <v-list>
-        <v-list-item 
-          v-for="item in addItems" 
-          :key="item.value" 
+        <v-list-item
+          v-for="item in menuItems"
+          :key="item.value"
           @click="handleItemClick(item)"
         >
           <template v-slot:prepend>
@@ -33,42 +33,48 @@
       </v-list>
     </v-navigation-drawer>
 
+    <!-- Bottom Navigation Bar -->
     <div class="relative h-24">
-      <div 
+      <!-- Floating Action Button -->
+      <div
         class="absolute left-1/2 transform -translate-x-1/2 bottom-0 mb-15 z-10"
       >
-        <div 
+        <div
           class="flex items-center justify-center rounded-full 
             w-16 h-16 bg-links"
         >
-          <v-btn 
-            @click="onPlusClick" 
+          <v-btn
+            @click="onPlusClick"
             variant="text"
             class="rounded-circle h-16 drop-shadow-lg"
           >
-            <v-icon class="text-white text-4xl">mdi-plus</v-icon>
+            <v-icon class="text-white text-4xl">{{ centerIcon }}</v-icon>
           </v-btn>
         </div>
       </div>
-      <div 
+
+      <!-- Navigation Buttons -->
+      <div
         class="absolute bottom-0 w-5/6 mb-8 left-1/2 transform 
           -translate-x-1/2 flex justify-between items-center 
           rounded-full py-2 px-8 bg-primary z-0 drop-shadow-lg"
       >
         <v-btn
-          @click="onHomeClick"
+          @click="onLeftButtonClick"
           :ripple="false"
           variant="text"
           class="text-white text-2xl"
-          icon="mdi-home" 
-        />
+        >
+          <v-icon>{{ leftIcon }}</v-icon>
+        </v-btn>
         <v-btn
-          @click="onSuggestClick"
+          @click="onRightButtonClick"
           :ripple="false"
           variant="text"
           class="text-white text-2xl"
-          icon="mdi-creation"
-        />
+        >
+          <v-icon>{{ rightIcon }}</v-icon>
+        </v-btn>
       </div>
     </div>
   </div>
@@ -77,18 +83,51 @@
 <script setup>
 import { ref } from 'vue';
 
+// Define props
+const props = defineProps({
+  // Icons for the buttons
+  centerIcon: {
+    type: String,
+    default: 'mdi-plus',
+  },
+  leftIcon: {
+    type: String,
+    default: 'mdi-home',
+  },
+  rightIcon: {
+    type: String,
+    default: 'mdi-creation',
+  },
+  // Menu items for the drawer
+  menuItems: {
+    type: Array,
+    default: () => [],
+  },
+});
+const { centerIcon, leftIcon, rightIcon, menuItems } = toRefs(props);
+
+// Emit events
+const emit = defineEmits([
+  'left-click',
+  'right-click',
+  'center-click',
+  'menu-item-click',
+]);
+
+// Component state
 const showDrawer = ref(false);
 
 const onPlusClick = () => {
   showDrawer.value = true;
+  emit('center-click');
 };
 
-const onHomeClick = () => {
-  navigateTo('/');
+const onLeftButtonClick = () => {
+  emit('left-click');
 };
 
-const onSuggestClick = () => {
-  console.log('Creation button clicked');
+const onRightButtonClick = () => {
+  emit('right-click');
 };
 
 const onExitClick = () => {
@@ -97,44 +136,17 @@ const onExitClick = () => {
 
 const handleItemClick = (item) => {
   showDrawer.value = false;
-  if (item.value === 'search') {
-    navigateTo('/books/search');
-  } else if (item.value === 'scan') {
-    // navigateTo('/scan');
-  } else if (item.value === 'manual') {
-    // navigateTo('/add');
-  }
+  emit('menu-item-click', item);
 };
-
-const addItems = [
-  {
-    title: 'Search by title',
-    value: 'search',
-    icon: 'mdi-magnify',
-  },
-  {
-    title: 'Scan barcode',
-    value: 'scan',
-    icon: 'mdi-barcode-scan',
-  },
-  {
-    title: 'Add book manually',
-    value: 'manual',
-    icon: 'mdi-book-plus',
-  },
-];
 </script>
 
 <style scoped>
 .drawer {
   border-radius: 1rem;
   margin-bottom: 2rem;
-  z-index: 1004 !important; /* Ensure the drawer is above the overlay */
-}
-.drawer-closed {
-  bottom: -100px !important;
+  z-index: 1004 !important;
 }
 .v-navigation-drawer__scrim {
-  position: fixed !important
+  position: fixed !important;
 }
 </style>
