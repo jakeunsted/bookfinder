5a7583275a9247b5e1e1d61a04a8ecf8778ed795
jakeunsted <jake.unsted@gmail.com>
2024-10-06 21:39:57 +0100

    capital components

---
+++ frontend/components/navigation/CloseBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-background h-16 content-center">
+    <v-icon
+      class="text-primary text-4xl pl-7"
+      @click="closeAndEmit"
+    >mdi-close</v-icon>
+  </div>
+</template>
+
+<script setup>
+const emit = defineEmits(['closeBarClicked']);
+
+const closeAndEmit = () => {
+  emit('closeBarClicked');
+};
+</script>
