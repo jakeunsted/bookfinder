032786a0d89b48f3bfc837745ff62ed2b929b8c6
Jake Unsted <jake.unsted@gmail.com>
2024-11-06 22:42:45 +0000

    rename app

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'nuxt-app',
+  appId: 'io.nextchapterai.app',
+  appName: 'NextChapterAI',
   webDir: 'dist',
   plugins: {
     CapacitorCookies: {
